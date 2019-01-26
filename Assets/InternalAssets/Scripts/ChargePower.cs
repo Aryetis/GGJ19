b4e5e84c38401e07d01d5d271e08fed844cc4f30
Aryetis <aramirg@hotmail.fr>
2019-01-26 05:21:59 +0100

    charge V1

--- Assets/InternalAssets/Scripts/ChargePower.cs
+++ Assets/InternalAssets/Scripts/ChargePower.cs
@@ -5,34 +5,72 @@ using UnityEngine;
 public class ChargePower : MonoBehaviour
 {
     public float CostPerSecond;
+    public float Speed;
+    public float gravityWhileDashing = 0.5f;
 
     private PlayerMovement pm;
+    private bool chargeActive;
+    private Vector3 m_move;
+    private CharacterController CC;
 
     // Start is called before the first frame update
     void Start()
     {
         pm = GetComponent<PlayerMovement>();
+        CC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Button 2"))
+        if (Input.GetKeyDown("joystick button 2") && Energy.Value > 0.0f && !chargeActive)
         {
             pm.enabled = false;
             Time.timeScale = 0.2f;
         }
-        else if (Input.GetButtonUp("Button 2"))
+        else if (Input.GetKeyUp("joystick button 2") && !chargeActive)
         {
             Time.timeScale = 1.0f;
+            // TODO spawn FX
+            // TODO tweak stock camera speed value (to avoid catching up the cam while dashing)
+            chargeActive = true;
+        }
+
+        if (chargeActive)
+        {
+            if (Energy.Value <= 0.0f)
+                StopCharge();
+            else
+            {
+                // Move 
+                m_move = new Vector3(transform.forward.x, -gravityWhileDashing, transform.forward.z) * Speed * Time.deltaTime;
+                CC.Move(m_move);
+
+                // Drain energy
+                Energy.RemoveEnergy(CostPerSecond * Time.deltaTime);
+            }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (collision.gameObject.CompareTag("Ramp") || collision.gameObject.CompareTag("DestructibleWall"))
+        if (hit.gameObject.CompareTag("DestructibleWall"))
         {
-
+            // TODO Spawn FX
+            Destroy(hit.gameObject);
         }
+        else if (hit.gameObject.CompareTag("Ramp") || hit.gameObject.CompareTag("Floor"))
+            return;
+        else
+        {
+            StopCharge();
+        }
+    }
+
+    private void StopCharge()
+    {
+        // TODO restore stock camera speed value
+        chargeActive = false;
+        pm.enabled = true;
     }
 }
--- Assets/InternalAssets/Scripts/PlayerMovement.cs
+++ Assets/InternalAssets/Scripts/PlayerMovement.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float m_speed = 1.0f;
+    public float gravity = 1.0f;
+    public static float staticGravity;
 
     private Vector3 m_move;
     private CharacterController CC; 
     
     void Start()
     {
+        staticGravity = gravity;
         CC = GetComponent<CharacterController>();
     }
     
     void Update()
     {
         // Move 
-        m_move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_speed * Time.deltaTime ;
+        m_move = new Vector3(Input.GetAxis("Horizontal"), -gravity, Input.GetAxis("Vertical")) * m_speed * Time.deltaTime ;
         CC.Move(m_move);
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y-(gravity*Time.deltaTime), transform.position.z);
 
         // Look forward
         if (m_move != Vector3.zero)
-            transform.rotation = Quaternion.LookRotation(m_move);
+            transform.rotation = Quaternion.LookRotation(new Vector3(m_move.x, 0, m_move.z));
     }
 }
