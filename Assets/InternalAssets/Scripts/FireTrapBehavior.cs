7323c879f2c1e39328dd3ffd1b4719a255ea1a8b
Aryetis <aramirg@hotmail.fr>
2019-01-26 16:35:56 +0100

    added firetrap and doortrap

---
+++ Assets/InternalAssets/Scripts/FireTrapBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrapBehavior : MonoBehaviour
+{
+    private static PlayerDeath pd;
+
+    void Start()
+    {
+        pd = GameObject.Find("Player").GetComponent<PlayerDeath>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player")
+            || col.gameObject.CompareTag("Totem") && TotemBehavior.PlayerFusioned )
+        {
+            pd.killPlayer();
+        }
+    }
+}
