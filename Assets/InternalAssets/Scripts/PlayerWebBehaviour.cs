288bd98148ffab33b4872c3effb87eadc35eb320
Kemsou <clement.patalano@gmail.com>
2019-01-26 17:36:42 +0100

    WebPillar, a new dawn

---
+++ Assets/InternalAssets/Scripts/PlayerWebBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWebBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float maxDistanceWithWeb = 20;
+
+    private GameObject attachedPillar;
+    private float initialSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attachedPillar = null;
+        initialSpeed = GetComponent<PlayerMovement>().m_speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, attachedPillar.transform.position) >= maxDistanceWithWeb)
+        {
+            transform.position = transform.position - (attachedPillar.transform.position - transform.position);
+        }
+    }
+
+    public GameObject getAttachedPillar()
+    {
+        return attachedPillar;
+    }
+
+    public void attachPillar(GameObject pillar)
+    {
+        if(pillar.GetComponent<WebPillarBehaviour>() == null)
+        {
+            Debug.Log("ERROR ! Expecting a pillar");
+        }
+        attachedPillar = pillar;
+    }
+
+    public void detachPillar()
+    {
+        attachedPillar = null;
+    }
+
+}
---
+++ Assets/InternalAssets/Scripts/TriggerWebScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWebScript : MonoBehaviour
+{
+    [SerializeField]
+    private Side triggerSide;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponentInParent<WebPillarBehaviour>().playerTouchedTrigger(triggerSide);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponentInParent<WebPillarBehaviour>().playerExitTrigger(triggerSide);
+    }
+}
