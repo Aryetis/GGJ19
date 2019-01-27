bd6969dce9b119d06e693456654e1559e41ebc41
Aryetis <aramirg@hotmail.fr>
2019-01-27 12:08:22 +0100

    door prefab

--- Assets/InternalAssets/Scripts/DoorBehavior.cs
+++ Assets/InternalAssets/Scripts/DoorBehavior.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class DoorBehavior : TogglableInterface
 {
     private GameObject DoorLeaf;
+    private Vector3 upPosition, downPosition;
+    private bool movingUp, movingDown;
 
     void Start()
     {
         DoorLeaf = transform.Find("DoorLeaf").gameObject;
+        upPosition = transform.position;
+        downPosition = transform.position + Vector3.down * 12.0f;
+        movingUp = false;
+        movingDown = false;
+    }
+
+    private void Update()
+    {
+        if (movingUp)
+        {
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, upPosition, 0.02f);
+            transform.position = smoothedPosition;
+            if ((transform.position - upPosition).magnitude <= 0.02f)
+                movingUp = false;
+        }
+        else if (movingDown)
+        {
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, downPosition, 0.02f);
+            transform.position = smoothedPosition;
+            if ((transform.position - downPosition).magnitude <= 0.02f)
+                movingDown = false;
+        }
     }
 
     public override void ToggleOn()
     {
-        DoorLeaf.SetActive(false);
+        movingDown = true;
+        movingUp = false;
     }
 
     public override void ToggleOff()
     {
-        DoorLeaf.SetActive(true);
+        movingDown = false;
+        movingUp = true;
     }
 }
---
+++ Assets/InternalAssets/Scripts/LoadLevelBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelBox : MonoBehaviour
+{
+    public string sceneNameToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Totem"))
+        {
+            // TODO fate out anim
+            other.gameObject.GetComponent<TotemBehavior>().enabled = false;
+            Invoke("LoadNextLevel", 2.0f);
+        }
+        else if(other.gameObject.CompareTag("Player"))
+        {
+            // TODO print UI message
+            Debug.Log("plz gimme totem");
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(sceneNameToLoad);
+    }
+
+}
